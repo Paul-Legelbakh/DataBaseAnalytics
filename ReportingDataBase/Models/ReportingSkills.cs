b57da929b26633441bfad7eff3be5b65ced1f93f
unknown <immortalis82@gmail.com>
2018-01-30 12:47:05 +0200

    Created project

---
+++ ReportingDataBase/Models/ReportingSkills.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportingDataBase.Models
+{
+    public class ReportingSkills
+    {
+        public int ID { get; set; }
+        public DateTime? ReportingDate { get; set; }
+        public int SkillID { get; set; }
+        public int Count { get; set; }
+    }
+}
