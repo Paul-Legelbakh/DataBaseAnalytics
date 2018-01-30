880c50960e24f0ba83cc2790370ee0f7f57d8534
unknown <immortalis82@gmail.com>
2018-01-30 13:39:49 +0200

    added the method for work of reporting every day

--- DataBaseAnalytics/Global.asax.cs
+++ DataBaseAnalytics/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +17,33 @@ namespace DataBaseAnalytics
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Start timer with start project
+            Thread thread = new Thread(new ThreadStart(Time));
+            thread.IsBackground = true;
+            thread.Name = "Time";
+            thread.Start();
+        }
+
+        //Work of timer every day
+        protected void TimerEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (DateTime.Now.Hour == 0)
+            {
+                DateTime dateReporting = DateTime.Now.AddDays(-1);
+                //call method
+            }
+        }
+
+        //Timer configuration
+        protected void Time()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(TimerEvent);
+            timer.Interval = 3600000;
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Start();
         }
     }
 }
